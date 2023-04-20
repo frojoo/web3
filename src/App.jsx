92d848a34588122144b0e9f8c2c6b0e904892374
frojoo <wndyd29605@gmail.com>
2023-04-20 22:00:02 +0900

    call balance

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,66 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Web3 from "web3";
+import { TOKEN_ABI, TOKEN_ADDRESS } from "./web3.config";
 
 const web3 = new Web3(window.ethereum);
 
-function App() {
-  const [adress, setAdress] = useState("");
+const tokenContract = new web3.eth.Contract(TOKEN_ABI, TOKEN_ADDRESS);
 
-  const onClickAdress = async () => {
+function App() {
+  const [address, setAddress] = useState("");
+  const [balance, setBalance] = useState();
+
+  //Metamask 주소 불러오기
+  const onClickAddress = async () => {
     try {
       const accounts = await window.ethereum.request({
         method: "eth_requestAccounts",
       });
 
-      setAdress(accounts[0]);
+      setAddress(accounts[0]);
     } catch (error) {
       console.error(error);
     }
   };
 
   const onclickLogOut = () => {
-    setAdress("");
+    setAddress("");
   };
 
-  useEffect(() => {
-    console.log(window.ethereum);
-  });
+  //TOKEN 잔고 불러오기
+  const onClickBalance = async () => {
+    try {
+      if (!address || !tokenContract) return;
+
+      const getBalance = await tokenContract.methods.balanceOf(address).call();
+
+      setBalance(web3.utils.fromWei(getBalance));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="min-h-screen bg-slate-200 flex justify-center items-center">
-      {adress ? (
-        <div className="text-2xl">
-          {adress.substring(0, 4)}....{adress.substring(adress.length - 4)}{" "}
-          <button className="btn-style text-xl ml-4" onClick={onclickLogOut}>
-            LogOut
-          </button>
+      {address ? (
+        <div>
+          <div className="text-2xl text-slate-700">
+            {address.substring(0, 4)}....{address.substring(address.length - 4)}{" "}
+            <button className="btn-style text-xl ml-4" onClick={onclickLogOut}>
+              LogOut
+            </button>
+          </div>
+          <div className="flex items-center mt-4">
+            {balance && (
+              <div className="text-xl text-slate-700">{balance} tMATIC</div>
+            )}
+            <button className="btn-style ml-4" onClick={onClickBalance}>
+              Balance
+            </button>
+          </div>
         </div>
       ) : (
-        <button className="btn-style" onClick={onClickAdress}>
+        <button className="btn-style" onClick={onClickAddress}>
           <img
             className="w-12"
             src={`${process.env.PUBLIC_URL}/images/metamask.png`}
